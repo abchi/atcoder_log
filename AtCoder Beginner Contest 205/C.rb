f2c370a249e972e709958848406dd466d05aae0b
abchi <5568900+abchi@users.noreply.github.com>
2021-06-13 23:06:48 +0900

    refactor: ABC205 Cをリファクタリング

--- AtCoder Beginner Contest 205/C.rb
+++ AtCoder Beginner Contest 205/C.rb
@@ -1,23 +1,16 @@
 a, b, c = gets.split.map(&:to_i)
 
-result = if c.even?
-           a = a.abs
-           b = b.abs
-           if a > b
-             '>'
-           elsif a < b
-             '<'
-           else
-             '='
-           end
+if c.even?
+  a = a.abs
+  b = b.abs
+end
+
+result = if a > b
+           '>'
+         elsif a < b
+           '<'
          else
-           if a > b
-             '>'
-           elsif a < b
-             '<'
-           else
-             '='
-           end
+           '='
          end
 
 puts result
